12391c5fa87a138cb58ccbc97e315eca457cfb2a
kenmaro3 <miha.ken.19@gmail.com>
2023-08-09 18:54:34 +0900

    Initial Commit

---
+++ SimpleModal/SimpleModalApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleModalApp.swift
+//  SimpleModal
+//
+//  Created by Kentaro Mihara on 2023/08/09.
+//
+
+import SwiftUI
+
+@main
+struct SimpleModalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
